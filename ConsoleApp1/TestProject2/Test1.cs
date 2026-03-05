7f9c79fca9df4e3fa6c4c3b7a148b5ae32da1208
Yuliia Pavlishak <juliapjo3@gmail.com>
2026-03-05 13:09:26 +0100

    phone testy

--- ConsoleApp1/TestProject2/Test1.cs
+++ ConsoleApp1/TestProject2/Test1.cs
@@ -1,11 +1,74 @@
-﻿namespace TestProject2
+﻿using ClassLibrary;
+namespace TestProject2
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class PhoneTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void Kostruktor_DanePoprawne()
         {
+            //Arrange
+            string owner = "Pavlishak";
+            string phoneNumber = "123456789";
+            Phone phone = new Phone(owner, phoneNumber);
+
+            //Act & Assert
+            Assert.AreEqual("Pavlishak", phoneNumber);
+            Assert.AreEqual("123456789", owner);
+ 
+
+            [TestMethod]
+        public void Kostruktor_DaneNiepoprawne_OwnerPusty()
+        {
+            //Arrange
+            string owner = "";
+            string phoneNumber = "123456789";
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => new Phone(owner, phoneNumber));
+        }
+        [TestMethod]
+        public void Kostruktor_DaneNiepoprawne_OwnerNull()
+        {
+            //Arrange
+            string owner = null;
+            string phoneNumber = "123456789";
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => new Phone(owner, phoneNumber));
+        }
+        [TestMethod]
+        public void Kostruktor_DaneNiepoprawne_NumerTelefonuNull()
+        {
+            //Arrange
+            string owner = "Pavlishak";
+            string phoneNumber = null;
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => new Phone(owner, phoneNumber));
+        }
+        [TestMethod]
+        [DataRow("123")]
+        [DataRow("12345678901233456797")]
+        public void Kostruktor_DaneNiepoprawne_NumerTelefonuZlejDlugosci(string nrTel)
+        {
+            //Arrange
+            string owner = "Pavlishak";
+            string phoneNumber = nrTel;
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => new Phone(owner, phoneNumber));
+        }
+        [TestMethod]
+        [DataRow("la123658670")]
+        [DataRow("1234567890.")]
+        public void Kostruktor_DaneNiepoprawne_NumerTelefonuNieTylkoCyfry(string nrTel)
+        {
+            //Arrange
+            string owner = "Pavlishak";
+            string phoneNumber = nrTel;
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => new Phone(owner, phoneNumber));
         }
     }
 }
